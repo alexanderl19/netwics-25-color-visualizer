879b30c8ac0b86ae11e4645dce14f31c0bd45336
Alexander Liu <a@alexanderliu.com>
2025-05-18 03:51:49 -0700

    feat: display color grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import type { Component } from "solid-js";
+import { createSignal, For, type Component } from "solid-js";
 
-import logo from "./logo.svg";
 import styles from "./App.module.css";
+import { createReconnectingWS } from "@solid-primitives/websocket";
+import { makePersisted } from "@solid-primitives/storage";
 
 const App: Component = () => {
+	const [colors, setColors] = makePersisted(
+		createSignal<{
+			[id: string]: { color: string; name?: string };
+		}>({}),
+		{ name: "colors" },
+	);
+
+	const ws = createReconnectingWS("ws://localhost:3000");
+	// const addMessage = (msg: WSMessage) => {
+	// 	setMessages(messages.length, msg);
+	// 	setTimeout(() => messageBox()?.lastElementChild?.scrollIntoView(), 50);
+	// };
+	ws.addEventListener("message", (msg) => {
+		const message = String(msg.data);
+		const [command, color, id] = message.split(" ");
+		if (command === "color") {
+			if (id) {
+				setColors({ ...colors, [id]: { color: color } });
+			}
+		}
+	});
+	ws.send("server");
+
 	return (
 		<div class={styles.App}>
-			<header class={styles.header}>
-				<img src={logo} class={styles.logo} alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					class={styles.link}
-					href="https://github.com/solidjs/solid"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn Solid
-				</a>
-			</header>
+			<div class={styles.Grid}>
+				<For each={Object.entries(colors())}>
+					{([id, { name, color }], i) => {
+						return (
+							<div class={styles.Cell} style={{ "background-color": color }}>
+								<div class={styles.Tag}>{name ?? id}</div>
+							</div>
+						);
+					}}
+				</For>
+			</div>
+			<div></div>
 		</div>
 	);
 };
